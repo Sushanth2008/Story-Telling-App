caca21b69881e86d1d31502c3193d4183ee47bba
Sushanth2008 <67271099+Sushanth2008@users.noreply.github.com>
2021-11-27 17:49:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC99Fv7XXiejjTb3nLkdfZjSjw9Kt2EU7w",
+    authDomain: "story-telling-app-915d5.firebaseapp.com",
+    databaseURL: "https://story-telling-app-915d5-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-915d5",
+    storageBucket: "story-telling-app-915d5.appspot.com",
+    messagingSenderId: "594682995909",
+    appId: "1:594682995909:web:240520dc736c7e93207cac"
+  };
